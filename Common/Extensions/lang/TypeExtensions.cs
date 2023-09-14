d3128eb567f4b4356d5938447a64c40142f2b591
Reformat <Reformat@foxmail.com>
2023-09-14 18:00:20 +0800

    【Feat】：新增Type类型扩展

--- Common/Extensions/lang/TypeExtensions.cs
+++ Common/Extensions/lang/TypeExtensions.cs
@@ -1,6 +1,24 @@
-﻿namespace Reformat.Framework.Core.Common.Extensions.lang;
+﻿using System.Reflection;
 
-public class TypeExtensions
+namespace Reformat.Framework.Core.Common.Extensions.lang;
+
+public static class TypeExtensions
 {
+    public static TypeInfo GetTypeInfo(this Type type)
+    {
+        if (type == (Type) null) throw new ArgumentNullException(nameof (type));
+        return type is IReflectableType reflectableType ? reflectableType.GetTypeInfo() : (TypeInfo) new TypeDelegator(type);
+    }
     
+    public static bool IsImplFrom(this Type entityType, Type interfaceType)
+    {
+        /*entityType.IsClass && !entityType.IsAbstract &&*/
+        return  entityType.GetTypeInfo().GetInterfaces()
+            .Any(t => t.GetTypeInfo().IsGenericType && t.GetGenericTypeDefinition() == interfaceType);
+    }
+
+    public static bool IsSubClassOf(this Type entityType, Type superType)
+    {
+        return entityType.GetTypeInfo().IsSubclassOf(superType);
+    }
 }
